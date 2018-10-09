a5422900cb5d61f3e302dc0cd4c22d3964c5a9ab
Elisangela Santos <elisangeladanielle@yahoo.com.br>
2018-10-09 18:38:35 -0300

    Atualizando o 9!

--- Codigo/ex9.R
+++ Codigo/ex9.R
@@ -1,3 +1,6 @@
+packrat::status()
+packrat::restore()
+
 library(readxl)
 
 dados <- read_excel("Dados/exercicio9.xls")
@@ -7,9 +10,15 @@ tabela = table(dados)
 #View(tabela)
 
 valores = dados$Salários
+range(valores)
+breaks <- seq(3, 25, by=2)
+
+valores.cut <- cut(valores, breaks, right=FALSE)
+
+frequencias = table(valores.cut)
+
+View(frequencias)
 
 png(filename = "Graficos/ex9.png", width = 500, height = 500)
 hist(x = valores, col = "green" ,main = "Salário dos Funcionários", xlab = "Salários", ylab="Funcionários")
 dev.off()
-
-#xlim=range(2)
